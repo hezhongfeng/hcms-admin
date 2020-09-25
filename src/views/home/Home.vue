bd4f8959d714cb892864e5f5aa48e98b1e855003
hezhongfeng <287645587@qq.com>
2020-09-25 16:06:26 +0800

    feat: 添加初始文件

---
+++ src/views/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">Home页</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.home {
+}
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.login {
+}
+</style>
