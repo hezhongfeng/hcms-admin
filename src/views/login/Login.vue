84e957347604d05f8bb6034da2df4583ce7bfd9f
hezhongfeng <287645587@qq.com>
2020-09-29 10:43:51 +0800

    feat(login): form校验

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="login">
-    <a-button type="primary">
-      Primary
-    </a-button>
-    <a-button>Default</a-button>
-    <a-button type="dashed">
-      Dashed
-    </a-button>
-    <a-button type="danger">
-      Danger
-    </a-button>
-    <a-config-provider :auto-insert-space-in-button="false">
-      <a-button type="primary">
-        按钮
-      </a-button>
-    </a-config-provider>
-    <a-button type="primary">
-      按钮
-    </a-button>
-    <a-button type="link">
-      Link
-    </a-button>
+    <a-form-model ref="ruleForm" :model="ruleForm" :rules="rules" v-bind="layout">
+      <a-form-model-item has-feedback label="账号" prop="username">
+        <a-input v-model="ruleForm.username" autocomplete="off" />
+      </a-form-model-item>
+      <a-form-model-item has-feedback label="密码" prop="password">
+        <a-input v-model="ruleForm.password" type="password" autocomplete="off" />
+      </a-form-model-item>
+      <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+        <a-button type="primary" @click="submitForm('ruleForm')">
+          登录
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
   </div>
 </template>
 
@@ -30,16 +22,60 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    let validateUsername = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入账号'));
+      } else {
+        callback();
+      }
+    };
+    let validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: 'change' }],
+        password: [{ validator: validatePassword, trigger: 'change' }]
+      },
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 }
+      }
+    };
   },
   computed: {},
   created() {},
   mounted() {},
-  methods: {}
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .login {
+  .ant-form {
+    width: 800px;
+  }
 }
 </style>
